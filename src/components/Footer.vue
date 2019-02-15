579e39c7dfba0065b15106d5bc73e75ab49e47ab
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-02-14 23:17:56 -0600

    Updated footer with links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,15 @@
 <template>
 	<footer class="footer">
-		<p class="footer__text">
-			Copyright &copy; {{ new Date().getFullYear() }}
-			<a href="http://alecrios.com">Alec Rios</a>
-		</p>
+		<div class="footer__text">
+			<div class="footer__item">
+				&copy; {{ new Date().getFullYear() }}
+				<a href="https://alecrios.com" target="_blank" rel="noopener">Alec Rios</a>
+			</div>
+
+			<div class="footer__item">
+				<a href="https://alecrios.com/contact" target="_blank" rel="noopener">Send Feedback</a>
+			</div>
+		</div>
 	</footer>
 </template>
 
@@ -15,14 +21,26 @@ export default {
 
 <style scoped>
 .footer {
-	text-align: center;
 	padding: 1.5rem;
 }
 
 .footer__text {
+	display: flex;
+	justify-content: center;
+}
+
+.footer__item {
 	font-size: .75rem;
 	font-weight: 400;
 	letter-spacing: .03125rem;
 	color: rgba(255, 255, 255, .5);
+}
+
+.footer__item + .footer__item {
+	margin-left: 1.5rem;
+}
+
+.footer__item a:hover {
+	text-decoration: underline;
 }
 </style>
